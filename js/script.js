18dddb52f9af12532d1381c8e1312a118bf3afee
javohir7777 <91466315+javohir7777@users.noreply.github.com>
2023-06-26 06:54:58 +0500

    full project

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 const Close = document.querySelector(".close");
 const Header = document.querySelector("header");
+const menu = document.querySelector(".menu");
+const Btn = document.querySelector(".navbar__textBtn");
 Close.addEventListener("click", () => {
   Header.style.display = "none";
+});
+menu.addEventListener("click", () => {
+  Btn.classList.toggle("block");
 });
